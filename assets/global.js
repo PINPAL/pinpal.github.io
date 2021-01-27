872c6fd33baf1a03a749ad1c30a0da8cb9764bef
PINPAL <18197217+PINPAL@users.noreply.github.com>
2021-01-27 11:12:43 +0000

    Clean Up Logs

--- assets/global.js
+++ assets/global.js
@@ -1,20 +1,31 @@
 //Function to create a HTML element
-function createElement(Name,Type) {
-    var element = document.createElement(Type)
-    if (Name != "") {
-        element.className = Name
-    }
-    return(element)
+function createElement(Name, Type) {
+  var element = document.createElement(Type);
+  if (Name != "") {
+    element.className = Name;
+  }
+  return element;
 }
 
 //Function to read XML Files
 function loadXMLDoc(filename) {
-    if (window.XMLHttpRequest) {
-        xhttp=new XMLHttpRequest();
-    } else {
-        xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhttp.open("GET",filename,false);
-    xhttp.send();
-    return xhttp.responseText;
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+  } else {
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.open("GET", filename, false);
+  xhttp.send();
+  return xhttp.responseText;
+}
+
+// hi developer
+console.log(
+  "Hi there, if your reading this then you're a cool dude!\nHowever, there is something really really important I need to tell you..\nTo continue you must enter the secret passcode..\n\n\n\n\n\n\nP.S: It's superSecretCode()"
+);
+
+function superSecretCode() {
+  console.log(
+    "It appears you have found the super secret!\n\nWell Unfortunetly I am here to tell you it's really not that interesting.\nThis is just a note from my future self to inform you that you absolutely SHOULD NOT look at the source code for this website. It's an absolute mess and needs to be rewritten. I apologise for any brain injuries that may occur if you dare read the source code.\n \nYours sincerely,\nPINPAL"
+  );
 }
--- assets/homepage/javascript.js
+++ assets/homepage/javascript.js
@@ -1,154 +1,187 @@
-
 // Decode Packages File into Object Array
-packages = decodePackagesFile(loadXMLDoc("https://pinpal.github.io/Packages"))
-console.log(packages)
+packages = decodePackagesFile(loadXMLDoc("https://pinpal.github.io/Packages"));
 // Render Packages File
 for (i = 0; i < packages.length; i++) {
-    var color = ""
-    // Create Wrapper Link
-    var a = document.createElement("a")
-    if (packages[i].hasOwnProperty("SileoDepiction")) {
-        a.href = "https://pinpal.github.io/Sileo-Depiction-WebViews/"
-                    + "?json="      + packages[i].SileoDepiction
-                    + "&name="      + packages[i].Name
-                    + "&section="   + packages[i].Section
-                    + "&dev="       + packages[i].Author
-        color = JSON.parse(loadXMLDoc(packages[i].SileoDepiction)).tintColor
-    } else {
-        a.href = packages[i].Depiction
-    }
-    // Create Bigbox
-    var bigBox = document.createElement("div")
-    bigBox.className = "bigBox"
-    // Create Package Content Holder
-    var packageContentHolder = document.createElement("div")
-    packageContentHolder.className = "packageContentHolder"
-    // Create Tweak Name
-    var packageTitle = document.createElement("span")
-    packageTitle.className = "packageTitle"
-    packageTitle.innerText = packages[i].Name
-    // Create Tweak Description
-    var packageDescription = document.createElement("span")
-    packageDescription.className = "packageDescription"
-    packageDescription.innerText = packages[i].Description
-    // Append Name and Description to Content Holder
-    packageContentHolder.appendChild(packageTitle)
-    packageContentHolder.innerHTML += "</br>"
-    packageContentHolder.appendChild(packageDescription)
-    // Create Package Icon
-    var packageIconHolder = document.createElement("div")
-    packageIconHolder.className = "packageIconHolder"
-    if (color != "") {
-        packageIconHolder.style.background = color
-    }
-    var img = document.createElement("img")
-    img.className = "packageicon"
-    img.src = "assets/page-icons/" + (packages[i].Name).replace(/ |-|:|;/g, "").toLowerCase() + ".png"
-    packageIconHolder.appendChild(img)
-    // Append bigBox to a
-    bigBox.appendChild(packageIconHolder)
-    bigBox.appendChild(packageContentHolder)
-    a.appendChild(bigBox)
-    // Add to Scroller
-    document.getElementById("scrollerRepo").appendChild(a)
+  var color = "";
+  // Create Wrapper Link
+  var a = document.createElement("a");
+  if (packages[i].hasOwnProperty("SileoDepiction")) {
+    a.href =
+      "https://pinpal.github.io/Sileo-Depiction-WebViews/" +
+      "?json=" +
+      packages[i].SileoDepiction +
+      "&name=" +
+      packages[i].Name +
+      "&section=" +
+      packages[i].Section +
+      "&dev=" +
+      packages[i].Author;
+    color = JSON.parse(loadXMLDoc(packages[i].SileoDepiction)).tintColor;
+  } else {
+    a.href = packages[i].Depiction;
+  }
+  // Create Bigbox
+  var bigBox = document.createElement("div");
+  bigBox.className = "bigBox";
+  // Create Package Content Holder
+  var packageContentHolder = document.createElement("div");
+  packageContentHolder.className = "packageContentHolder";
+  // Create Tweak Name
+  var packageTitle = document.createElement("span");
+  packageTitle.className = "packageTitle";
+  packageTitle.innerText = packages[i].Name;
+  // Create Tweak Description
+  var packageDescription = document.createElement("span");
+  packageDescription.className = "packageDescription";
+  packageDescription.innerText = packages[i].Description;
+  // Append Name and Description to Content Holder
+  packageContentHolder.appendChild(packageTitle);
+  packageContentHolder.innerHTML += "</br>";
+  packageContentHolder.appendChild(packageDescription);
+  // Create Package Icon
+  var packageIconHolder = document.createElement("div");
+  packageIconHolder.className = "packageIconHolder";
+  if (color != "") {
+    packageIconHolder.style.background = color;
+  }
+  var img = document.createElement("img");
+  img.className = "packageicon";
+  img.src =
+    "assets/page-icons/" +
+    packages[i].Name.replace(/ |-|:|;/g, "").toLowerCase() +
+    ".png";
+  packageIconHolder.appendChild(img);
+  // Append bigBox to a
+  bigBox.appendChild(packageIconHolder);
+  bigBox.appendChild(packageContentHolder);
+  a.appendChild(bigBox);
+  // Add to Scroller
+  document.getElementById("scrollerRepo").appendChild(a);
 }
 
 //Backup content of original scrollers (for un-duplicating on expand)
-var originalScrollersRepo = document.getElementById("scrollerRepo").innerHTML
-var originalScrollersProjects = document.getElementById("scrollerProjects").innerHTML
+var originalScrollersRepo = document.getElementById("scrollerRepo").innerHTML;
+var originalScrollersProjects = document.getElementById("scrollerProjects")
+  .innerHTML;
 
 //Function to calculate bigBox size
 function calculateBoxSize() {
-    if (window.innerWidth < 900) {
-        bigBoxWidth = 99
-        bigBoxAnimationDuration = 1.7
-    } else {
-        bigBoxWidth = 436
-        bigBoxAnimationDuration = 4
-    }
+  if (window.innerWidth < 900) {
+    bigBoxWidth = 99;
+    bigBoxAnimationDuration = 1.7;
+  } else {
+    bigBoxWidth = 436;
+    bigBoxAnimationDuration = 4;
+  }
 }
 
 //About Me Button
-var topBanner = document.getElementById("topBanner")
+var topBanner = document.getElementById("topBanner");
 function expandAbout() {
-    topBanner.getElementsByTagName("button")[0].style.maxHeight = "0"
-    topBanner.getElementsByTagName("button")[0].style.opacity = "0"
-    topBanner.getElementsByTagName("button")[0].style.margin = "0"
-    topBanner.getElementsByTagName("h1")[0].innerText = "Josh"
-    topBanner.getElementsByTagName("h2")[0].style.height = "0"
-    document.getElementById("hiddenInfo").style.maxHeight = "5000px"
-    document.getElementById("hiddenInfo").classList.add("animate")
-    topBanner.getElementsByTagName("img")[0].classList.add("animate")
-    setTimeout(function(){
-        topBanner.getElementsByTagName("img")[0].src = "assets/homepage/animoji.png"
-    }, 300);
+  topBanner.getElementsByTagName("button")[0].style.maxHeight = "0";
+  topBanner.getElementsByTagName("button")[0].style.opacity = "0";
+  topBanner.getElementsByTagName("button")[0].style.margin = "0";
+  topBanner.getElementsByTagName("h1")[0].innerText = "Josh";
+  topBanner.getElementsByTagName("h2")[0].style.height = "0";
+  document.getElementById("hiddenInfo").style.maxHeight = "5000px";
+  document.getElementById("hiddenInfo").classList.add("animate");
+  topBanner.getElementsByTagName("img")[0].classList.add("animate");
+  setTimeout(function () {
+    topBanner.getElementsByTagName("img")[0].src =
+      "assets/homepage/animoji.png";
+  }, 300);
 }
 
 //Touch Display Detection
-window.addEventListener('touchstart', function onFirstTouch() {
-    console.log("tapped")
-    var touchScreenStylesheet = document.createElement("link")
-    touchScreenStylesheet.type = "text/css"
-    touchScreenStylesheet.rel = "stylesheet"
-    touchScreenStylesheet.href = "assets/homepage/touchscreenOnly.css"
-    document.head.appendChild(touchScreenStylesheet)
-    window.removeEventListener('touchstart', onFirstTouch, false);
-  }, false);
+window.addEventListener(
+  "touchstart",
+  function onFirstTouch() {
+    var touchScreenStylesheet = document.createElement("link");
+    touchScreenStylesheet.type = "text/css";
+    touchScreenStylesheet.rel = "stylesheet";
+    touchScreenStylesheet.href = "assets/homepage/touchscreenOnly.css";
+    document.head.appendChild(touchScreenStylesheet);
+    window.removeEventListener("touchstart", onFirstTouch, false);
+  },
+  false
+);
 
 //Function to create animations
 function calculateAnimationWidthOf(classID) {
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = '@keyframes ' + classID + ' {100% {transform: translateX(-' + (document.getElementById(classID).innerHTML.match(/bigBox/g).length/4) * bigBoxWidth +  'px); }}';
-    document.getElementsByTagName('head')[0].appendChild(style);
+  var style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML =
+    "@keyframes " +
+    classID +
+    " {100% {transform: translateX(-" +
+    (document.getElementById(classID).innerHTML.match(/bigBox/g).length / 4) *
+      bigBoxWidth +
+    "px); }}";
+  document.getElementsByTagName("head")[0].appendChild(style);
 }
 
 //Set width to width of BigBoxes function
 function setWidth(classID) {
-    calculateBoxSize()
-    document.getElementById(classID).style.minWidth = document.getElementById(classID).innerHTML.match(/bigBox/g).length * bigBoxWidth + "px";
-    document.getElementById(classID).style.animation = classID + " " + (document.getElementById(classID).innerHTML.match(/bigBox/g).length/4) * bigBoxAnimationDuration + "s linear infinite";
-    calculateAnimationWidthOf(classID)
+  calculateBoxSize();
+  document.getElementById(classID).style.minWidth =
+    document.getElementById(classID).innerHTML.match(/bigBox/g).length *
+      bigBoxWidth +
+    "px";
+  document.getElementById(classID).style.animation =
+    classID +
+    " " +
+    (document.getElementById(classID).innerHTML.match(/bigBox/g).length / 4) *
+      bigBoxAnimationDuration +
+    "s linear infinite";
+  calculateAnimationWidthOf(classID);
 }
 
 //Duplicating Function
 function duplicateContentOf(classID) {
-    document.getElementById(classID).innerHTML = document.getElementById(classID).innerHTML +
-                                                                                         document.getElementById(classID).innerHTML +
-                                                                                         document.getElementById(classID).innerHTML +
-                                                                                         document.getElementById(classID).innerHTML
-    setWidth(classID)
+  document.getElementById(classID).innerHTML =
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML +
+    document.getElementById(classID).innerHTML;
+  setWidth(classID);
 }
 
 //Function to expand/collapse scrollers
 function expand(classID) {
-    if (document.getElementById(classID + 'Button').innerText == 'Show More') {
-        document.getElementById(classID + 'Container').style.maxHeight = "100%";
-        document.getElementById(classID).style.minWidth = "100%";
-        document.getElementById(classID + 'Button').innerHTML = '<img class="buttonIcon" src="/assets/homepage/collapse.png">Show Less';
-        if (classID == "scrollerRepo") {
-            document.getElementById(classID).innerHTML = originalScrollersRepo
-        } else if (classID == "scrollerProjects") {
-            document.getElementById(classID).innerHTML = originalScrollersProjects
-        }
-        document.getElementById(classID).style.animation = "none";
-    } else {
-        document.getElementById(classID + 'Container').style.maxHeight = "210px";
-        document.getElementById(classID + 'Button').innerHTML = '<img class="buttonIcon" src="/assets/homepage/expand.png">Show More';
-        duplicateContentOf(classID)
+  if (document.getElementById(classID + "Button").innerText == "Show More") {
+    document.getElementById(classID + "Container").style.maxHeight = "100%";
+    document.getElementById(classID).style.minWidth = "100%";
+    document.getElementById(classID + "Button").innerHTML =
+      '<img class="buttonIcon" src="/assets/homepage/collapse.png">Show Less';
+    if (classID == "scrollerRepo") {
+      document.getElementById(classID).innerHTML = originalScrollersRepo;
+    } else if (classID == "scrollerProjects") {
+      document.getElementById(classID).innerHTML = originalScrollersProjects;
     }
+    document.getElementById(classID).style.animation = "none";
+  } else {
+    document.getElementById(classID + "Container").style.maxHeight = "210px";
+    document.getElementById(classID + "Button").innerHTML =
+      '<img class="buttonIcon" src="/assets/homepage/expand.png">Show More';
+    duplicateContentOf(classID);
+  }
 }
 
 //Reload scrollers width on window resize
-window.onresize = function(event) {
-    if (document.getElementById("scrollerRepo").innerHTML != originalScrollersRepo) {
-        setWidth("scrollerRepo")
-    }
-    if (document.getElementById("scrollerProjects").innerHTML != originalScrollersProjects) {
-        setWidth("scrollerProjects")
-    }
-}
+window.onresize = function (event) {
+  if (
+    document.getElementById("scrollerRepo").innerHTML != originalScrollersRepo
+  ) {
+    setWidth("scrollerRepo");
+  }
+  if (
+    document.getElementById("scrollerProjects").innerHTML !=
+    originalScrollersProjects
+  ) {
+    setWidth("scrollerProjects");
+  }
+};
 
 //Duplicate and fit width of all scrollers
-duplicateContentOf("scrollerRepo")
-duplicateContentOf("scrollerProjects")
+duplicateContentOf("scrollerRepo");
+duplicateContentOf("scrollerProjects");
